ff05e40be6d3baa0d6ec57f43d744f3aad68db56
Veradux21 <spmat21@gmail.com>
2022-04-27 16:37:44 -0400

    Fixed xr switch, renamed script, cleaned logging and duplicate commands
    Found appropriate syntax for 7zip -xr switch
    Added validation for whether oldtested.log exists or not to prevent 7zip -xr 'file doesn't exist' fatal error and added Write-Output to indicate that
    Improved Write-Output at error and tested cleanup
    Renamed main script file
    Removed write to oldtested until full path is supported
    Removed a duplicate removal command to filter out "Testing Archive"

--- 7zipBatchValidator.ps1
+++ 7zipBatchValidator.ps1
@@ -1,5 +1,5 @@
 <#How to call from cli#>
-#pwsh E:\Assets\code.ps1
+#pwsh E:\Assets\7zipBatchValidator.ps1
 
 Set-PSDebug -Trace 0
 <#Because Powershell can't persist aliases-----#>
@@ -15,35 +15,42 @@ if ( $7zExists.lines -gt 0)
 Set-Location -Path "E:\Assets\"
 <#Set Width so it doesn't break Remove-Item----#>
 $PSDefaultParameterValues=@{"Out-File:Width"="10000"}
-<#Remove archive.log---------------------------#>
-Remove-Item .\archive.log -ErrorAction SilentlyContinue
-<#Clean error.log, Test archives, create log---#>
-Remove-Item .\error.log -ErrorAction SilentlyContinue
 Write-Output("Old Archive and Error removed. Start Test")
-7z t -air!*.7z <# -xr@oldtested.txt #> 2>&1 1>>.\archive.log
+If ((Get-Content -Path ./oldtested.txt | Measure-Object -Line | Select-Object -expand Lines) -gt 0){
+    7z t -an -air!*.7z -xr@'oldtested.txt' 2>&1 1>>.\archive.log
+    Write-Output("Not First Run, using oldtested.log")
+}else{
+    7z t -an -air!*.7z <# -xr@'oldtested.txt' #> 2>&1 1>>.\archive.log
+    Write-Output("First Run, no oldtested.log") 
+}
 Write-Output (Get-Content -Path ./archive.log | Measure-Object -Line)
 <#Filter out Errors, archive already tested----#>
 Select-String -Path ./archive.log -Pattern "ERROR.*:.*\.7z" -Raw > ./error.log
 Select-String -Path ./archive.log -Pattern "Testing archive: " -Raw > ./tested.log
-Write-Output (Get-Content ./error.log) 
 <#Clean logs#>
 (Get-Content ./error.log) -replace "ERROR: ", "" | Set-Content -Path ./error.log
 (Get-Content ./tested.log) -replace "Testing archive: ", "" | Set-Content -Path ./tested.log
-Write-Output (Get-Content ./error.log)
+$errorCount = Get-Content -Path ./error.log | Measure-Object -Line | Select-Object -expand Lines
+Write-Output "Cleaned Errors: $($errorCount)"
+$testedCount = Get-Content -Path ./tested.log | Measure-Object -Line | Select-Object -expand Lines
+Write-Output "Tested: $($testedCount)"
 <#Append error.log to oldbad.log---------------#>
 Get-Content -Path .\error.log | Add-Content -Path oldbad.log
 Write-Output("Appended error to oldbad")
 <#Filter out tested, format correctly, add to oldtested#>
-# Select-String -Path ./archive.log -Pattern "Testing archive: " -Raw > ./justtested.log
-(Get-Content ./tested.log) -replace "Testing archive: ", '' | Set-Content -Path ./justtested.log
-(Get-Content ./justtested.log).ForEach({ '"{0}"' -f $_ }) | Add-Content -Path ./oldtested.txt
+<# Commenting oldtested add-content until full path is supported on 7z exclusion flag#>
+#(Get-Content ./justtested.log).ForEach({ '"{0}"' -f $_ }) | Add-Content -Path ./oldtested.txt
 Write-Output("Added Quotes to oldtested")
 <#Take breath, then delete each object in error.log#>
 Start-Sleep -s 5
-If ((Get-Content -Path ./error.log | Measure-Object -Line | Select-Object -expand Lines) -gt 0){
+If ((Get-Content -Path ./error.log | Measure-Object -Line | Select-Object -expand Lines) -gt 0)
+{
     Get-Content -Path ./error.log | ForEach-Object {Remove-Item -Force -Recurse -LiteralPath "$_"}
-        Write-Output("Heresy Eliminated")
-    }else{
+    Write-Output("Heresy Eliminated")
+}else{
        Write-Output("No errors") 
     }
-    
+<#Remove archive.log---------------------------#>
+Remove-Item .\archive.log -ErrorAction SilentlyContinue
+<#Clean error.log, Test archives, create log---#>
+Remove-Item .\error.log -ErrorAction SilentlyContinue
--- README.md
+++ README.md
@@ -4,15 +4,24 @@ Powershell script to test archives, log the good ones, and log then delete the b
 Outline:
 1. Set Alias for 7zip. Should match default install location if installed on C:\
 2. Set location to the root folder with the bulk archives. Directories within this folder are expected.
-3. Set Powershell Out-File default width to 10k so Out-file doesn't wordwrap your path names
-4. Archive and error files from previous run (once debugging complete, move to bottom of the script)
-5. Run 7zip archive test. (Need to figure out why the exclusion listfile argument isn't working... would significantly improve repeat runs)
-6. Filter Errors only for .7z files into a log for deletion. Filter all files tested into "tested.log"
-7. Clean up logs of tested and error so that they reflect the literal path
-8. Sleep
-9. Check to see if there are errors. If so, forcibly and recursively delete all erroneous 7z files.
+3. Set Powershell Out-File default width to 10k so Out-file doesn't word-wrap your path names
+4. Run 7zip archive test.
+5. Filter Errors only for .7z files into a log for deletion. Filter all files tested into "tested.log"
+6. Clean up logs of tested and error so that they reflect the literal path
+7. Sleep
+8. Check to see if there are errors. If so, forcibly and recursively delete all erroneous 7z files.
+9. Archive and error files from previous run
 
 Feature Enhancements:
 
 - [ ] Support user entered root domain.
 - [ ] Push user submission into 7z call, clean up, Remove-Item.... etc
+- [X] Add listfile argument to 7zip call to ignore already scanned files
+- [X] Move Step 4 (Remove-Item Archive.log and Error.log) to end of script
+- [ ] Resolve why "#" folder isn't being ignored even though its in the exclusion file
+
+
+Found appropriate syntax for 7zip -xr switch
+Added validation for whether oldtested.log exists or not to prevent 7zip -xr 'file doesn't exist' fatal error and added Write-Output to indicate that
+Improved Write-Output at error and tested cleanup
+Renamed main script file
